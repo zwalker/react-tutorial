bcb6c236182d23aaa69cc01f55c60b50b06a2a0d
Zach Walker <zach.walker@appfolio.com>
2016-10-07 14:59:27 -0700

    zw - load comments from server when component is mounted"

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,10 +1,28 @@
 // 1
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -61,13 +79,8 @@ var Comment = React.createClass({
   }
 });
 
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *antoher* comment"}
-];
-
 ReactDOM.render(
-  <CommentBox data={data}/>,
+  <CommentBox url="/api/comments" />,
   document.getElementById('content')
 );
 
